ad39442752b60fb6cc33c6d07092c5f216dba33e
tekkitak <tekkitakcz@gmail.com>
2022-08-03 00:59:48 +0200

    Init

---
+++ main.py
@@ -0,0 +1,3 @@
+h,j,k,l=map(int,map(input,['']*4))
+i=-(-(l-k)//(h-j))
+print(f"{i},{i*(h+j)}")
---
+++ tests.bat
@@ -0,0 +1,6 @@
+@echo off
+printf 2\n1\n0\n10 | python main.py
+printf 5\n3\n6\n20 | python main.py
+printf 1\n2\n12\n0 | python main.py
+printf 3\n2\n1\n101 | python main.py
+printf 4\n17\n1000\n101 | python main.py
